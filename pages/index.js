36c0121df992aedd1b9fe2d64eff03a0fbcb9f72
suravarapupadma095-beep <suravarapupadma095@gmail.com>
2026-06-10 14:04:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,39 @@
 export default function Home() {
   return (
-    <div style={{padding:'40px',fontFamily:'Arial'}}>
-      <h1>ðŸ“– StoryBookVerse</h1>
-      <p>A world of stories, mysteries, and adventures.</p>
+    <div style={{
+      backgroundColor: "#0a0a0a",
+      color: "white",
+      minHeight: "100vh",
+      padding: "40px",
+      fontFamily: "Arial"
+    }}>
+      
+      <h1 style={{
+        fontSize: "50px",
+        color: "#8b5cf6"
+      }}>
+        STORYBOOKVERSE
+      </h1>
 
-      <div style={{marginTop:'20px'}}>
-        <a href="/stories">Stories</a>
-      </div>
+      <p style={{
+        fontSize: "20px",
+        maxWidth: "600px"
+      }}>
+        Enter the world of suspense, mystery and legends.
+      </p>
 
-      <div style={{marginTop:'10px'}}>
-        <a href="/dashboard">Dashboard</a>
-      </div>
+      <button style={{
+        marginTop: "20px",
+        padding: "15px 30px",
+        backgroundColor: "#8b5cf6",
+        border: "none",
+        color: "white",
+        borderRadius: "10px",
+        fontSize: "18px"
+      }}>
+        Read Stories
+      </button>
 
-      <div style={{marginTop:'10px'}}>
-        <a href="/admin">Admin Panel</a>
-      </div>
     </div>
   )
 }
